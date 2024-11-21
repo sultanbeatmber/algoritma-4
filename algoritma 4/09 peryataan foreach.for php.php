528e6789bad2d1b5e15c9d6e045efeb83cdf0790
sultanbeatmber <fauzansultan656@gmail.com>
2024-11-21 14:20:56 +0700

    penulian

--- algoritma 4/09 peryataan foreach.for php.php
+++ algoritma 4/09 peryataan foreach.for php.php
@@ -3,4 +3,4 @@ $warna = array('merah', 'hijau', 'biru');
 foreach ($warna as $w) {
   echo "Warna: $w";
 }
-echo "<i><p><strong> By : Arif alfazhar";
+echo "<i><p><strong> By : sultan beat mber";
---
+++ algoritma 4/10 penulisan for pada.php
@@ -0,0 +1,26 @@
+<?php
+
+for ($i = 1; $i <= 10; $i++) {
+    echo $i;
+}
+echo "<hr>";
+
+for ($i = 1;; $i++) {
+    if ($i > 10) {
+        break;
+    }
+    echo $i;
+}
+echo "<hr>";
+$i = 1;
+for (;;) {
+    if ($i > 10) {
+        break;
+    }
+    echo $i;
+    $i++;
+}
+echo "<hr>";
+for ($i = 1; $i <= 10; print $i, $i++);
+echo "<hr>";
+echo "<i><p><strong> By : sultan beat mber";
