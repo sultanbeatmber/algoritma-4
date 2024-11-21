1908a6abb1c5f0e5be3e9305177502d1370fed96
sultanbeatmber <fauzansultan656@gmail.com>
2024-11-21 11:59:16 +0700

    02 perulnagan while.php

---
+++ algoritma 4/perulngan 02.php
@@ -0,0 +1,18 @@
+<?php
+
+
+    // blok kode yang akan diulang di sini    
+
+
+?>
+Contoh:
+<?php 
+
+$ulangi = 0;
+
+while($ulangi < 10){
+    echo "<p>Ini adalah perulangan ke-$ulangi</p>";
+    $ulangi++;
+}
+echo "<i><p><strong> By : sultan beat mber";
+?>
