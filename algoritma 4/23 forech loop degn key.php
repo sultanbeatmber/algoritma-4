b90ae1b1094736578b327d126e2fac9758d6c231
sultanbeatmber <fauzansultan656@gmail.com>
2024-11-23 13:23:17 +0700

    commit algoritma 4

---
+++ algoritma 4/23 forech loop degn key.php
@@ -0,0 +1,7 @@
+<?php
+$harga = array("apel" => 10000, "pisang" => 5000, "ceri" => 15000);
+
+foreach ($harga as $kunci => $nilai) {
+    echo "Harga dari " . $kunci . " adalah " . $nilai . "<br>";
+}
+echo "<i><p><strong> By : sultan";
