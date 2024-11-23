af20465f8dbbe190968f6f0ec47f8b5ee32842a3
sultanbeatmber <fauzansultan656@gmail.com>
2024-11-23 13:32:26 +0700

    commit algoritma 4

---
+++ algoritma 4/26 penggunaan countinue.php
@@ -0,0 +1,10 @@
+<?php
+
+for ($i = 0; $i < 10; $i++) {
+    if ($i % 2 === 0) {
+        continue;
+    }
+    echo $i . " ";
+}
+// Output: 1 3 5 7 9
+echo "<i><p><strong> By : sultan";
---
+++ algoritma 4/27 break pada swicth.php
@@ -0,0 +1,19 @@
+<?php
+
+$buah = "apel";
+
+switch ($buah) {
+    case "mangga":
+        echo "Ini mangga";
+        break;
+    case "apel":
+        echo "Ini apel";
+        // Break disini menghentikan switch
+        break;
+    case "pisang":
+        echo "Ini pisang";
+        break;
+    default:
+        echo "Buah tidak dikenal";
+}
+echo "<i><p><strong> By : sultan";
