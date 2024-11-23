739bc70096c5f250e839f225d140144818568963
sultanbeatmber <fauzansultan656@gmail.com>
2024-11-23 13:14:43 +0700

    commit algoritma4

---
+++ algoritma 4/19 penanganan array dengan while loop.php
@@ -0,0 +1,10 @@
+<?php
+$fruits = ["apple", "banana", "cherry"];
+$i = 0;
+
+while ($i < count($fruits)) {
+    echo $fruits[$i] . "<br>";
+    $i++;
+}
+echo "<i><p><strong> By : Arif alfazhar";
+?>
