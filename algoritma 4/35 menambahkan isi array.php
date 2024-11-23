371cc09d316de88da36f617bd3d3293fac6533fa
sultanbeatmber <fauzansultan656@gmail.com>
2024-11-23 13:48:02 +0700

    commit algortima 4

---
+++ algoritma 4/35 menambahkan isi array.php
@@ -0,0 +1,8 @@
+<?php
+$email["subjek"] = "Apa Kabar?";
+$email["pengirim"] = "arifksp@gmail.com";
+$email["isi"] = "Apa kabar? sudah lama tidak berjumpa";
+
+echo "<pre>";
+print_r($email);
+echo "</pre>";
